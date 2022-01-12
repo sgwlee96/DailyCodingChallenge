b27020d8640de3c0c091f75d88c1fcb517dc93af
sgwlee96 <sgwlee@ucdavis.edu>
2022-01-12 18:17:20 +0900

    DataStructure1 Day1-2

---
+++ DataStructure1_Day1_2.py
@@ -0,0 +1,6 @@
+# Problem : 53. Maximum Subarray
+# Given an integer array nums, find the contiguous subarray (containing at least one number) 
+# which has the largest sum and return its sum.
+# A subarray is a contiguous part of an array.
+
+num = []
