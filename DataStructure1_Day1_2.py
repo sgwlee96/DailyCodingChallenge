dce2df7d26fa51bdc57edc19a779083753a2ae42
sgwlee96 <sgwlee@ucdavis.edu>
2022-01-13 18:16:47 +0900

    DataStructure1 Day1-2

--- DataStructure1_Day1_2.py
+++ DataStructure1_Day1_2.py
@@ -3,4 +3,19 @@
 # which has the largest sum and return its sum.
 # A subarray is a contiguous part of an array.
 
-num = []
+
+# Solution
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(1, len(nums)):
+            if nums[i-1] > 0:
+                nums[i] += nums[i-1]
+        return max(nums)
+
+
+# Runtime: 854 ms, faster than 24.05% of Python online submissions for Maximum Subarray.
+# Memory Usage: 25.7 MB, less than 25.26% of Python online submissions for Maximum Subarray.
